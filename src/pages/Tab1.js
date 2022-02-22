4aaebfc38c7caa7047db914c7c63252e916281b4
Mark Howe <mvahowe@gmail.com>
2022-02-22 13:27:21 +0100

    Simple query example

--- src/pages/Tab1.js
+++ src/pages/Tab1.js
@@ -2,50 +2,19 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar,} from '@ionic/react';
 
-import {useQuery} from 'proskomma-react-hooks';
 
 import './Tab1.css';
 
 export default function Tab1({pkState, isLoaded}) {
     const [result, setResult] = useState({});
-    const {
-        stateId,
-        // newStateId,
-        proskomma,
-        errors: proskommaErrors,
-        verbose,
-    } = pkState;
-
-    const tab1Query = '{ id docSets { id } }';
-
-    const {
-        stateId: queryStateId,
-        query: queryRun,
-        data,
-        errors: queryErrors,
-    } = useQuery({
-        proskomma,
-        stateId,
-        tab1Query,
-        verbose,
-    });
 
     useEffect(
         () => {
-            proskomma.gqlQuery(tab1Query).then(res => setResult(res));
+            const tab1Query = '{ id nDocSets }';
+            pkState.proskomma.gqlQuery(tab1Query).then(res => setResult(res));
         },
-        [proskomma, isLoaded]
+        [pkState.proskomma, isLoaded]
     );
-
-    const dataStringified = JSON.stringify({
-        stateId,
-        queryStateId,
-        tab1Query,
-        queryRun,
-        proskommaErrors,
-        queryErrors,
-        data,
-    }, null, 2);
 
     return (
         <IonPage>
@@ -57,16 +26,25 @@ export default function Tab1({pkState, isLoaded}) {
             <IonContent fullscreen>
                 <IonGrid>
                     <IonRow>
-                        <IonCol>
-                            {isLoaded ? "Loaded" : "Not Loaded"} {JSON.stringify(result.data)}
-                        </IonCol>
+                        <IonCol>{
+                                isLoaded ?
+                                    'Loaded' :
+                                    'Not Loaded'
+                            }</IonCol>
                     </IonRow>
                     <IonRow>
-                        <IonCol>
-                            <span style={{whiteSpace: 'pre'}}>
-                                {dataStringified}
-                            </span>
-                        </IonCol>
+                        <IonCol>{
+                                result.data ?
+                                    <pre>{JSON.stringify(result.data, null, 2)}</pre> :
+                                    "No Data"
+                            }</IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>{
+                                result.errors ?
+                                    <pre>{JSON.stringify(result.errors, null, 2)}</pre> :
+                                    "No Errors"
+                            }</IonCol>
                     </IonRow>
                 </IonGrid>
             </IonContent>
@@ -76,8 +54,8 @@ export default function Tab1({pkState, isLoaded}) {
 
 Tab1.propTypes = {
     pkState: PropTypes.shape({
-        stateId: PropTypes.string.isRequired,
-        newStateId: PropTypes.func.isRequired,
+        stateId: PropTypes.string,
+        newStateId: PropTypes.func,
         proskomma: PropTypes.object.isRequired,
         errors: PropTypes.arrayOf(
             PropTypes.string,
