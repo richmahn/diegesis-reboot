4e60c3658a81b8e5dc336857c0547178c1ea7431
Mark Howe <mvahowe@gmail.com>
2022-03-02 09:45:54 +0100

    IonSelect demo in PageHeader

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,26 +1,50 @@
-import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import {IonCol, IonGrid, IonHeader, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar} from "@ionic/react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 
-export default function PageHeader({ title, navState }) {
-  // To do 1. Add drop down selectors for Bible, Book, Chapter , verse
-  // 2. Use navState to show current value
-  // 3. Use setNavState to set new value on change
-  // 4. Add Next, previous buttons as per the tab ie next chapter /previous chapter on chapters tab
-  // 5. Hide Book, chapter, verse dropdown as per tab
+export default function PageHeader({title, navState}) {
+    // To do 1. Add drop down selectors for Bible, Book, Chapter , verse
+    // 2. Use navState to show current value
+    // 3. Use setNavState to set new value on change
+    // 4. Add Next, previous buttons as per the tab ie next chapter /previous chapter on chapters tab
+    // 5. Hide Book, chapter, verse dropdown as per tab
 
-  console.log(navState);
-  return (
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle size="large">{title}</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-  );
+    const [color, setColor] = useState("red");
+
+    console.log(navState);
+    return (
+        <IonHeader>
+            <IonToolbar>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonTitle size="large">{title}</IonTitle>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol size={3}>
+                            <IonSelect
+                                value={color}
+                                okText="Change"
+                                cancelText="Cancel"
+                                onIonChange={e => setColor(e.detail.value)}
+                            >
+                                <IonSelectOption value="brown">Brown</IonSelectOption>
+                                <IonSelectOption value="blonde">Blonde</IonSelectOption>
+                                <IonSelectOption value="black">Black</IonSelectOption>
+                                <IonSelectOption value="red">Red</IonSelectOption>
+                            </IonSelect>
+                        </IonCol>
+                        <IonCol size={3}>{color}</IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonToolbar>
+        </IonHeader>
+    );
 }
 
 PageHeader.propTypes = {
-  title: PropTypes.string,
-  navState: PropTypes.object.isRequired,
-  setNavState: PropTypes.func.isRequired,
+    title: PropTypes.string,
+    navState: PropTypes.object.isRequired,
+    setNavState: PropTypes.func.isRequired,
 };
