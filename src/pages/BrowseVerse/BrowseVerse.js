6f9f04cb17870357e70fe96291be0bfafb7b7813
tombogle <tom_bogle@sil.org>
2022-03-02 09:57:03 -0500

    [WIP] First crack at getting BrowseVerse to display a verse.

--- src/pages/BrowseVerse/BrowseVerse.js
+++ src/pages/BrowseVerse/BrowseVerse.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import {useQuery} from "proskomma-react-hooks";
 import PropTypes from "prop-types";
-import {IonPage} from '@ionic/react';
+import {IonCol, IonContent, IonGrid, IonPage, IonRow} from '@ionic/react';
 import PageHeader from "../../components/PageHeader";
-import StubPageContent from "../../components/StubPageContent";
 
 import './BrowseVerse.css';
 
@@ -11,30 +11,52 @@ export default function BrowseVerse({pkState}) {
     const query = '{' +
     '  docSet(id:"xyz-spa_rv09") {' +
     '    id' +
-    '    document(bookCode:"GAL") {' +
-    '      cv(chapter:"5") {' +
-    '        text' +
-    '        tokens {' +
-    '          payload' +
-    '        }' +
-    '        items {' +
-    '          type' +
-    '          subType' +
-    '          payload' +
-    '        }' +
+    '    document(bookCode:"MAT") {' +
+    '      mainSequence {' +
+    '        blocks(withScriptureCV:"6:9") {' +
+    '            items{type subType payload}' +
+    '       }' +
     '      }' +
     '    }' +
     '  }' +
     '}';
 
+    const verbose=true;
+
+    const queryState = useQuery({
+         ...pkState,
+         query,
+         verbose,
+     });
+
+    const renderParagraphContents = b => b.items.map((b, n) => {
+        if (b.type === 'token')
+        {
+            return <span className={'c' + n}>{b.payload}</span>;
+        }
+        else if (b.type === 'scope' && b.subType === 'start' && b.payload.startsWith('verse/'))
+        {
+            return <h1 key={n}>{'MAT 6:' + b.payload.split('/')[1]}</h1>;
+        }
+    }
+    );
+
+    const renderVerse = (b, n) => {
+         return <p key={n}>{renderParagraphContents(b)}</p>
+    };
+
     return (
         <IonPage>
             <PageHeader title="Browse Verse" />
-            <StubPageContent
-                pkState={pkState}
-                query={query}
-                description="Show details of one verse. The current query shows the verse content as text, as tokens and as items."
-            />
+            <IonContent>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            {queryState.data.docSet && queryState.data.docSet.document.mainSequence.blocks.map((b, n) => renderVerse(b, n))}
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
         </IonPage>
     );
 }
