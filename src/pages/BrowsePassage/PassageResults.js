ac37cd8cc45e9c8a368a0add8f23fb1b0c7b0afb
eliaspinero <eliasrpt14@gmail.com>
2022-03-21 13:29:31 -0430

    Added new conditionals for blocks.

--- src/pages/BrowsePassage/PassageResults.js
+++ src/pages/BrowsePassage/PassageResults.js
@@ -4,6 +4,7 @@ import {IonCol, IonRow} from '@ionic/react';
 import PassageByVersions from "./PassageByVersions";
 import PassageByVerse from "./PassageByVerse";
 import PassageByVersion from "./PassageByVersion";
+import PkDataAsJson from "../../components/PkDataAsJson";
 
 export default function PassageResults({reference, parseResult, docSets, displayFlags}) {
     const cvArray = docSets[0]?.document.cv.map(v => v.scopeLabels) || [];
@@ -32,12 +33,16 @@ export default function PassageResults({reference, parseResult, docSets, display
                 Verse not found!
             </IonCol>
         </IonRow>;
-    } else if (displayFlags.groupVerses){
-        return <PassageByVerse cvArray={cvArray} docSets={docSets} />;
-    } else if (docSets.length === 1){
-        return docSets.map((ds, n) => <PassageByVersion key={n} docSet={ds} keyPrefix={n} />);
-    } else {
-        return <PassageByVersions docSets={docSets} />;
+    } else if (displayFlags.byBlock) {
+        return <PkDataAsJson data={docSets} />
+    } else {    // by Verse
+        if (displayFlags.groupVerses){
+            return <PassageByVerse cvArray={cvArray} docSets={docSets} />;
+        } else if (docSets.length === 1){
+            return docSets.map((ds, n) => <PassageByVersion key={n} docSet={ds} keyPrefix={n} />);
+        } else {
+            return <PassageByVersions docSets={docSets} />;
+        }
     }
 }
 
